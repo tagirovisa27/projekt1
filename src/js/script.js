fc318ec6eda4cff291b9913c81c61c89b9e88e04
isa <isatagirov01@mail.ru>
2023-08-09 16:53:23 +0300

    lesson-7

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector("#burger");
+const closer = document.querySelector("#close");
+
+const menu  = document.querySelector('#mob_menu');
+
+burger.addEventListener('click', function(){
+    menu.style.left = 0
+})
+
+closer.addEventListener('click', function(){
+    menu.style.left = "100%"
+})
