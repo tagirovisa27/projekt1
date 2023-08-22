e36e3ef46ed914335f2ffd22e48470ca357ed8b1
isa <isatagirov01@mail.ru>
2023-08-22 15:43:20 +0300

    lesson js

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,45 @@
-const burger = document.querySelector("#burger");
+let num = [];
 
-const menu  = document.querySelector('#mob_menu');
-
-const closer = document.querySelector("#close");
-
-burger.addEventListener('click', function(){
-    menu.style.left = 0
-})
-closer.addEventListener('click', function(){
-    menu.style.left = "100%"
-})
-
-let a = [];
-
-for (let i = 2; i <= 1000; i += 2) {
-    a.push(i);
+for(let i = 0; i <= 1000; i+=2) {
+  num.push(i);
+   console.log(i);
 }
 
-console.log();
+  let numTwo = [];
+
+   for (let i = 0; i < num.length; i++){
+    if (num [i] % 3 === 0) {
+     numTwo.push(num [i] ** 2);
+     
+  }
+} 
+
+console.log(numTwo);
+
+let index = 10; 
+
+for (let i = 0; i < numTwo.length; i++) {
+  if (numTwo[i] === 324) {
+    index = i;
+  }
+}
+
+console.log(index);
+
+index = 3;
+
+console.log(index);
+
+
+
+const methods = [3,6,9,];
+
+methods.push(12);  
+
+a = methods.pop(1);
+
+methods.unshift(10);
+
+b = methods.shift(12);
+
+console.log(methods,a,b);
